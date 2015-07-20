f3c28caac9ad7f636cdba72b98e3ece4f00ff88f
Philippe Luchansky <philuchansky@gmail.com>
2015-07-20 00:03:25 -0700

    add category column add icons for categories

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150720023547) do
+ActiveRecord::Schema.define(version: 20150720063537) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20150720023547) do
     t.text   "description"
     t.string "homepage"
     t.text   "samplecode"
+    t.text   "category"
   end
 
 end
